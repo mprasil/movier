1201589bac03ba269a597823190dfff11df2a919
Miroslav Prasil <miroslav@prasil.info>
2016-03-10 13:08:14 +0000

    Initial import

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(
+    name="movier",
+    version="0.2.0",
+
+    author="Miroslav Prasil",
+    author_email="miroslav@prasil.info",
+    url="https://github.com/mprasil/movier",
+
+    include_package_data=True,
+
+    license="GPLv2 or later",
+    description="Move movies to your library with proper name and subtitle encoding.",
+
+
+    install_requires=[
+        "chardet",
+    ],
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+
+    ],
+
+    scripts=["movier"]
+
+)
+
